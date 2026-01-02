6d8cc114363c739c6cc2016c4f5c5cae718ce809
Dhanushgowdamv <dhanushvenu199@gmail.com>
2026-01-03 00:28:20 +0530

    db-setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+import {ENV} from "./env.js"
+
+export const connectDB = async()=>{
+    try {
+         const conn = await mongoose.connect(ENV.DB_URL)
+         console.log("Connected to mongoose", conn.connection.host)
+    } catch (error){
+       console.error("error connecting to mongo ",error);
+       process.exit(1);
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import path from "path";
 import {ENV} from "./lib/env.js"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 console.log(ENV.PORT)
 console.log(ENV.DB_URL)
 
-const __dirname = path.resolve()
+const __dirname = path.resolve();
 
-app.get("/",(req,res)=>{
+app.get("/health",(req,res)=>{
     res.status(200).json({msg:"sucess form api"})
+
+});
+app.get("/books",(req,res)=>{
+    res.status(200).json({msg:"sucess form api bookfh"})
 
 });
  
@@ -22,4 +27,15 @@ if(ENV.NODE_ENV === 'production'){
     })
 }
 
-app.listen(ENV.PORT,()=>console.log("server is riunning",ENV.PORT))
+
+
+const startserver = async () =>{
+    try{
+        await connectDB();
+        app.listen(ENV.PORT,()=>console.log("server is riunning",ENV.PORT));
+    }catch (error){
+       console.error("error starting teh server ", error);
+    }
+}
+
+startserver();
