f834900284ce6d307155bb05e9892f90d14ed7c3
Dhanushgowdamv <dhanushvenu199@gmail.com>
2026-01-04 16:58:54 +0530

    Resolve merge conflict in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,58 +1,56 @@
 import express from "express";
 import path from "path";
-import cors from "cors";
-import { serve } from "inngest/express";
-
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
 import { inngest, functions } from "./lib/inngest.js";
 
 const app = express();
+console.log(ENV.PORT);
+console.log(ENV.DB_URL);
+
 const __dirname = path.resolve();
 
 // middleware
 app.use(express.json());
-app.use(cors({
-  origin: ENV.CLIENT_URL,
-  credentials: true
-}));
+// server allows the browser to include the cookies in the request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-// Inngest endpoint ✅ (IMPORTANT: missing / before api fixed)
-app.use("/api/inngest", serve({
-  client: inngest,
-  functions
-}));
+app.use(
+  "api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
 
-// health routes
 app.get("/health", (req, res) => {
   res.status(200).json({ msg: "success from api" });
 });
 
 app.get("/books", (req, res) => {
-  res.status(200).json({ msg: "success from api bookfh" });
+  res.status(200).json({ msg: "sucess from api bookfh" });
 });
 
-// serve frontend in production
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.join(__dirname, "../frontend", "dist", "index.html")
-    );
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-// start server
-const startServer = async () => {
+const startserver = async () => {
   try {
     await connectDB();
     app.listen(ENV.PORT, () =>
-      console.log("Server running on port", ENV.PORT)
+      console.log("server is riunning", ENV.PORT)
     );
   } catch (error) {
-    console.error("Error starting server:", error);
+    console.error("error starting teh server ", error);
   }
 };
 
-startServer();
+startserver();
